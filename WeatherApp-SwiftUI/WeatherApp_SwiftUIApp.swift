5325e7ee1a9f63cc9c9b902b7d3a963813a4334e
Muhammad Abdul Ghani <ghani28muhammad@gmail.com>
2024-02-18 19:50:01 +0700

    Initial Commit

---
+++ WeatherApp-SwiftUI/WeatherApp_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp_SwiftUIApp.swift
+//  WeatherApp-SwiftUI
+//
+//  Created by Muhammad Abdul Ghani on 18/02/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
